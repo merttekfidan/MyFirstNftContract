5935a03b8e3f6c34c5783c65c610d355bb66c6f6
Merdjan Tekfidan <merttekfidan@gmail.com>
2022-01-10 23:55:45 +0100

    Opensea implement

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -1,10 +1,25 @@
 // SPDX-License-Identifier: UNLICENSED
 
 pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
 import "hardhat/console.sol";
 
-contract MyEpicNFT {
-    constructor() {
+contract MyEpicNFT is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721 {
         console.log("This is my first NFT Contract !!!");
+    }
+
+    function makeAnEpicNFT() public {
+        uint256 newItemId = _tokenIds.current();
+        _safeMint(msg.sender, newItemId);
+
+        _setTokenURI(newItemId, "Blah");
+
+        _tokensId.increment();
     }
 }
