7dd5605759e2183915e08285d2356c9a39d79a4b
Merdjan Tekfidan <merttekfidan@gmail.com>
2022-01-11 23:59:39 +0100

    Implement working version

--- scripts/run.js
+++ scripts/run.js
@@ -1,9 +1,16 @@
 
-const main = async()=>{
+const main = async ()=>{
     const nftContractFactory = await hre.ethers.getContractFactory('MyEpicNFT');
     const nftContract = await nftContractFactory.deploy();
     await nftContract.deployed();
     console.log('NFT contract deployed to : ',nftContract.address);
+
+    let txn = await nftContract.makeAnEpicNFT();
+    await txn.wait();
+
+    txn = await nftContract.makeAnEpicNFT();
+    await txn.wait();
+
 }
 
 const runMain = async ()=>{
